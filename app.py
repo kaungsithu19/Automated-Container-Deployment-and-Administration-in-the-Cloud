98e079a61e1881c9ebc0841bb337784c8c8ae133
Kaung Si Thu <157499734+kaungsithu19@users.noreply.github.com>
2024-11-29 10:10:45 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, World! This is a Flask Docker app for Task-4."
+    return "Hello, World! Task-4 is DONEEE!!!."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
